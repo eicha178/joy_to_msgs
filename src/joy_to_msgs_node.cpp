ab5c60d680ba93ab66437d91571497851a3fc0c2
eicha178 <eicha178@gmail.com>
2018-12-23 14:58:25 +0900

    update joy_to_msgs_node

--- src/joy_to_msgs_node.cpp
+++ src/joy_to_msgs_node.cpp
@@ -2,12 +2,16 @@
 #include "sensor_msgs/Joy.h"
 #include "geometry_msgs/Twist.h"
 
-geometry_msgs::Twist twist;
+geometry_msgs::Twist msg_track;
+geometry_msgs::Twist msg_turret;
 
 void joyCallback(const sensor_msgs::Joy& msg)
 {
-    twist.linear.x = msg.axes[1];
-    twist.angular.z = msg.axes[0];
+    msg_track.linear.x = -msg.axes[1];
+    msg_track.angular.z = msg.axes[0];
+
+    msg_turret.angular.y = -msg.axes[5];
+    msg_turret.angular.z =  msg.axes[2];
 }
 
 int main(int argc, char **argv)
@@ -17,7 +21,8 @@ int main(int argc, char **argv)
 
     ros::Subscriber joy_sub = n.subscribe("joy", 1000, joyCallback);
 
-    ros::Publisher twist_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
+    ros::Publisher track_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
+    ros::Publisher turret_pub = n.advertise<geometry_msgs::Twist>("cmd_turret", 1000);
 
     ros::Rate loop_rate(10);
 
@@ -25,7 +30,8 @@ int main(int argc, char **argv)
     {
         //std::stringstream ss;
 
-        twist_pub.publish(twist);
+        track_pub.publish(msg_track);
+        turret_pub.publish(msg_turret);
 
         ros::spinOnce();
         loop_rate.sleep();
